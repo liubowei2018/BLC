837d5d84376e97088dc859b8781e285d65bfbc86
liubowei0114 <15039157606@163.com>
2019-01-14 13:46:52 +0800

    add upgrade

--- application/api/controller/Usermoney.php
+++ application/api/controller/Usermoney.php
@@ -163,6 +163,8 @@ class Usermoney extends Base
             }
             //增加团队业绩 和 今日业绩
             Db::query('CALL TeamPerformance('.$user_info['id'].','.$data['number'].')');
+            //查询上级是否满足升级
+            $this->upgrade($user_info['id'],$user_info['uuid'],$data['number']);
             Db::commit();
             return json(['code'=>1011,'msg'=>'买入成功','data'=>[]]);
         }catch (\Exception $exception){
@@ -260,6 +262,35 @@ class Usermoney extends Base
 
     }
 
+    public function ceshi(){
+        $this->upgrade(2,2,2);
+    }
+    /**
+     * 用户升级  零级升一级
+     * @param $userid  父级ID
+     * @param $user_uuid
+     * @param $money
+     */
+    private function upgrade($user_id,$user_uuid,$money){
+        $uset_team = Db::name('member')->where('pid',$user_id)->select();
+        $ConfigUpgrade = Db::name('config_upgrade')->where('lv',1)->find();
+        $user_grade = Db::name('member_grade')->where('mid',$user_id)->find();
+        $satisfy = 0;//满足条件的个数
+        if($uset_team && $user_grade['level'] == 0){
+            foreach ($uset_team as $k=>$v){
+                //查询配置信息
+                if($v['team_money'] >= $ConfigUpgrade['money']){
+                    $satisfy = $satisfy + 1;
+                }
+            }
+            if($satisfy >= $ConfigUpgrade['number']){
+                // 满足条件的直推大于等于 升级
+                $result = Db::name('member_grade')->where('mid',$user_id)->update(['level'=>1]);
+                // 同时给上级添加一个等级
+                Db::query("CALL TeamUpgrade($user_id,1)");
+            }
+        }
+    }
 
     /**
      * 定期买入记录
